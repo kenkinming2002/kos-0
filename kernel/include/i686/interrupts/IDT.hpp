9edd1af5371655f8f7b9e2810c8da48340894c5f
KenKwok <kenkinming2002@gmail.com>
2021-02-03 22:06:15 +0800

    Refactor interrupts code and move compiler flags into meson.build file

--- kernel/include/i686/interrupts/IDT.hpp
+++ kernel/include/i686/interrupts/IDT.hpp
@@ -4,6 +4,20 @@
 
 namespace core::interrupts
 {
+  enum Mask
+  {
+    TYPE_MASK            = 0xF,
+    PRIVILEGE_LEVEL_MASK = 0x60,
+    PRESENT_MASK         = 0x80
+  };
+
+  enum Shift
+  {
+    TYPE_SHIFT = 0,
+    PRIVILEGE_LEVEL_SHIFT = 5,
+    PRESENT_SHIFT = 7
+  };
+
   enum class InterruptType
   {
     TASK_GATE_32      = 0x5,
@@ -16,12 +30,29 @@ namespace core::interrupts
   class IDTEntry
   {
   public:
-    IDTEntry() = default;
-    IDTEntry(InterruptType interruptType, PrivilegeLevel privillegeLevel, uint16_t selector, uintptr_t address, bool present = true);
+    constexpr IDTEntry() = default;
+    constexpr IDTEntry(InterruptType interruptType, PrivilegeLevel privilegeLevel, uint16_t selector, uintptr_t address, bool present = true)
+    {
+      m_typeAttr = static_cast<uint8_t>(interruptType)  << TYPE_SHIFT |
+                   static_cast<uint8_t>(privilegeLevel) << PRIVILEGE_LEVEL_SHIFT |
+                   static_cast<uint8_t>(present)        << PRESENT_SHIFT;
+      m_selector = selector;
+
+      m_offsetLow  = address & 0xFFFF;
+      m_offsetHigh = address >> 16;
+
+      m_zero = 0;
+    }
 
   public:
-    bool enabled() const;
-    void enabled(bool state);
+    constexpr bool present() const { return (m_typeAttr & PRESENT_MASK) >> PRESENT_SHIFT; }
+    constexpr void present(bool state) { m_typeAttr = (static_cast<uint8_t>(m_typeAttr) & ~PRESENT_MASK) | (static_cast<uint8_t>(state) << PRESENT_SHIFT); }
+
+    constexpr InterruptType type() const { return static_cast<InterruptType>((m_typeAttr & TYPE_MASK) >> TYPE_SHIFT); }
+    constexpr void type(InterruptType type) { m_typeAttr = (static_cast<uint8_t>(m_typeAttr) & ~TYPE_MASK) | (static_cast<uint8_t>(type) << TYPE_SHIFT); }
+
+    constexpr PrivilegeLevel privilegeLevel() const { return static_cast<PrivilegeLevel>((m_typeAttr & PRIVILEGE_LEVEL_MASK) >> PRIVILEGE_LEVEL_SHIFT); }
+    constexpr void privilegeLevel(PrivilegeLevel privilegeLevel) { m_typeAttr = (static_cast<uint8_t>(m_typeAttr) & ~PRIVILEGE_LEVEL_MASK) | (static_cast<uint8_t>(privilegeLevel) << PRIVILEGE_LEVEL_SHIFT); }
 
   private:
     uint16_t m_offsetLow  = 0; 
@@ -34,7 +65,11 @@ namespace core::interrupts
   struct IDT
   {
   public:
-    IDT(const IDTEntry(&idtEntries)[256]);
+    IDT(const IDTEntry(&idtEntries)[256])
+    {
+      m_size = sizeof idtEntries - 1;
+      m_offset = reinterpret_cast<uintptr_t>(idtEntries);
+    }
 
   private:
     uint16_t m_size;
--- kernel/src/i686/interrupts/Interrupts.cpp
+++ kernel/src/i686/interrupts/Interrupts.cpp
@@ -16,42 +16,42 @@
 namespace core::interrupts
 {
   constexpr static size_t IDT_SIZE = 256;
-  IDTEntry idtEntries[IDT_SIZE];
+  static inline auto& idtEntries()
+  {
+    static IDTEntry idtEntries[IDT_SIZE];
+    struct Initializer
+    {
+      Initializer()
+      {
+        using isr_t = void(*)();
+        static constexpr isr_t isrs[IDT_SIZE] = {
+          &isr0,   &isr1,   &isr2,   &isr3,   &isr4,   &isr5,   &isr6,   &isr7,   &isr8,   &isr9,   &isr10,  &isr11,  &isr12,  &isr13,  &isr14,  &isr15,
+          &isr16,  &isr17,  &isr18,  &isr19,  &isr20,  &isr21,  &isr22,  &isr23,  &isr24,  &isr25,  &isr26,  &isr27,  &isr28,  &isr29,  &isr30,  &isr31,
+          &isr32,  &isr33,  &isr34,  &isr35,  &isr36,  &isr37,  &isr38,  &isr39,  &isr40,  &isr41,  &isr42,  &isr43,  &isr44,  &isr45,  &isr46,  &isr47,
+          &isr48,  &isr49,  &isr50,  &isr51,  &isr52,  &isr53,  &isr54,  &isr55,  &isr56,  &isr57,  &isr58,  &isr59,  &isr60,  &isr61,  &isr62,  &isr63,
+          &isr64,  &isr65,  &isr66,  &isr67,  &isr68,  &isr69,  &isr70,  &isr71,  &isr72,  &isr73,  &isr74,  &isr75,  &isr76,  &isr77,  &isr78,  &isr79,
+          &isr80,  &isr81,  &isr82,  &isr83,  &isr84,  &isr85,  &isr86,  &isr87,  &isr88,  &isr89,  &isr90,  &isr91,  &isr92,  &isr93,  &isr94,  &isr95,
+          &isr96,  &isr97,  &isr98,  &isr99,  &isr100, &isr101, &isr102, &isr103, &isr104, &isr105, &isr106, &isr107, &isr108, &isr109, &isr110, &isr111,
+          &isr112, &isr113, &isr114, &isr115, &isr116, &isr117, &isr118, &isr119, &isr120, &isr121, &isr122, &isr123, &isr124, &isr125, &isr126, &isr127,
+          &isr128, &isr129, &isr130, &isr131, &isr132, &isr133, &isr134, &isr135, &isr136, &isr137, &isr138, &isr139, &isr140, &isr141, &isr142, &isr143,
+          &isr144, &isr145, &isr146, &isr147, &isr148, &isr149, &isr150, &isr151, &isr152, &isr153, &isr154, &isr155, &isr156, &isr157, &isr158, &isr159,
+          &isr160, &isr161, &isr162, &isr163, &isr164, &isr165, &isr166, &isr167, &isr168, &isr169, &isr170, &isr171, &isr172, &isr173, &isr174, &isr175,
+          &isr176, &isr177, &isr178, &isr179, &isr180, &isr181, &isr182, &isr183, &isr184, &isr185, &isr186, &isr187, &isr188, &isr189, &isr190, &isr191,
+          &isr192, &isr193, &isr194, &isr195, &isr196, &isr197, &isr198, &isr199, &isr200, &isr201, &isr202, &isr203, &isr204, &isr205, &isr206, &isr207,
+          &isr208, &isr209, &isr210, &isr211, &isr212, &isr213, &isr214, &isr215, &isr216, &isr217, &isr218, &isr219, &isr220, &isr221, &isr222, &isr223,
+          &isr224, &isr225, &isr226, &isr227, &isr228, &isr229, &isr230, &isr231, &isr232, &isr233, &isr234, &isr235, &isr236, &isr237, &isr238, &isr239,
+          &isr240, &isr241, &isr242, &isr243, &isr244, &isr245, &isr246, &isr247, &isr248, &isr249, &isr250, &isr251, &isr252, &isr253, &isr254, &isr255,
+        };
 
-  uintptr_t isrs[IDT_SIZE] = {
-    reinterpret_cast<uintptr_t>(&isr0),   reinterpret_cast<uintptr_t>(&isr1),   reinterpret_cast<uintptr_t>(&isr2),   reinterpret_cast<uintptr_t>(&isr3),   reinterpret_cast<uintptr_t>(&isr4),   reinterpret_cast<uintptr_t>(&isr5),   reinterpret_cast<uintptr_t>(&isr6),   reinterpret_cast<uintptr_t>(&isr7),
-    reinterpret_cast<uintptr_t>(&isr8),   reinterpret_cast<uintptr_t>(&isr9),   reinterpret_cast<uintptr_t>(&isr10),  reinterpret_cast<uintptr_t>(&isr11),  reinterpret_cast<uintptr_t>(&isr12),  reinterpret_cast<uintptr_t>(&isr13),  reinterpret_cast<uintptr_t>(&isr14),  reinterpret_cast<uintptr_t>(&isr15),
-    reinterpret_cast<uintptr_t>(&isr16),  reinterpret_cast<uintptr_t>(&isr17),  reinterpret_cast<uintptr_t>(&isr18),  reinterpret_cast<uintptr_t>(&isr19),  reinterpret_cast<uintptr_t>(&isr20),  reinterpret_cast<uintptr_t>(&isr21),  reinterpret_cast<uintptr_t>(&isr22),  reinterpret_cast<uintptr_t>(&isr23),
-    reinterpret_cast<uintptr_t>(&isr24),  reinterpret_cast<uintptr_t>(&isr25),  reinterpret_cast<uintptr_t>(&isr26),  reinterpret_cast<uintptr_t>(&isr27),  reinterpret_cast<uintptr_t>(&isr28),  reinterpret_cast<uintptr_t>(&isr29),  reinterpret_cast<uintptr_t>(&isr30),  reinterpret_cast<uintptr_t>(&isr31),
-    reinterpret_cast<uintptr_t>(&isr32),  reinterpret_cast<uintptr_t>(&isr33),  reinterpret_cast<uintptr_t>(&isr34),  reinterpret_cast<uintptr_t>(&isr35),  reinterpret_cast<uintptr_t>(&isr36),  reinterpret_cast<uintptr_t>(&isr37),  reinterpret_cast<uintptr_t>(&isr38),  reinterpret_cast<uintptr_t>(&isr39),
-    reinterpret_cast<uintptr_t>(&isr40),  reinterpret_cast<uintptr_t>(&isr41),  reinterpret_cast<uintptr_t>(&isr42),  reinterpret_cast<uintptr_t>(&isr43),  reinterpret_cast<uintptr_t>(&isr44),  reinterpret_cast<uintptr_t>(&isr45),  reinterpret_cast<uintptr_t>(&isr46),  reinterpret_cast<uintptr_t>(&isr47),
-    reinterpret_cast<uintptr_t>(&isr48),  reinterpret_cast<uintptr_t>(&isr49),  reinterpret_cast<uintptr_t>(&isr50),  reinterpret_cast<uintptr_t>(&isr51),  reinterpret_cast<uintptr_t>(&isr52),  reinterpret_cast<uintptr_t>(&isr53),  reinterpret_cast<uintptr_t>(&isr54),  reinterpret_cast<uintptr_t>(&isr55),
-    reinterpret_cast<uintptr_t>(&isr56),  reinterpret_cast<uintptr_t>(&isr57),  reinterpret_cast<uintptr_t>(&isr58),  reinterpret_cast<uintptr_t>(&isr59),  reinterpret_cast<uintptr_t>(&isr60),  reinterpret_cast<uintptr_t>(&isr61),  reinterpret_cast<uintptr_t>(&isr62),  reinterpret_cast<uintptr_t>(&isr63),
-    reinterpret_cast<uintptr_t>(&isr64),  reinterpret_cast<uintptr_t>(&isr65),  reinterpret_cast<uintptr_t>(&isr66),  reinterpret_cast<uintptr_t>(&isr67),  reinterpret_cast<uintptr_t>(&isr68),  reinterpret_cast<uintptr_t>(&isr69),  reinterpret_cast<uintptr_t>(&isr70),  reinterpret_cast<uintptr_t>(&isr71),
-    reinterpret_cast<uintptr_t>(&isr72),  reinterpret_cast<uintptr_t>(&isr73),  reinterpret_cast<uintptr_t>(&isr74),  reinterpret_cast<uintptr_t>(&isr75),  reinterpret_cast<uintptr_t>(&isr76),  reinterpret_cast<uintptr_t>(&isr77),  reinterpret_cast<uintptr_t>(&isr78),  reinterpret_cast<uintptr_t>(&isr79),
-    reinterpret_cast<uintptr_t>(&isr80),  reinterpret_cast<uintptr_t>(&isr81),  reinterpret_cast<uintptr_t>(&isr82),  reinterpret_cast<uintptr_t>(&isr83),  reinterpret_cast<uintptr_t>(&isr84),  reinterpret_cast<uintptr_t>(&isr85),  reinterpret_cast<uintptr_t>(&isr86),  reinterpret_cast<uintptr_t>(&isr87),
-    reinterpret_cast<uintptr_t>(&isr88),  reinterpret_cast<uintptr_t>(&isr89),  reinterpret_cast<uintptr_t>(&isr90),  reinterpret_cast<uintptr_t>(&isr91),  reinterpret_cast<uintptr_t>(&isr92),  reinterpret_cast<uintptr_t>(&isr93),  reinterpret_cast<uintptr_t>(&isr94),  reinterpret_cast<uintptr_t>(&isr95),
-    reinterpret_cast<uintptr_t>(&isr96),  reinterpret_cast<uintptr_t>(&isr97),  reinterpret_cast<uintptr_t>(&isr98),  reinterpret_cast<uintptr_t>(&isr99),  reinterpret_cast<uintptr_t>(&isr100), reinterpret_cast<uintptr_t>(&isr101), reinterpret_cast<uintptr_t>(&isr102), reinterpret_cast<uintptr_t>(&isr103),
-    reinterpret_cast<uintptr_t>(&isr104), reinterpret_cast<uintptr_t>(&isr105), reinterpret_cast<uintptr_t>(&isr106), reinterpret_cast<uintptr_t>(&isr107), reinterpret_cast<uintptr_t>(&isr108), reinterpret_cast<uintptr_t>(&isr109), reinterpret_cast<uintptr_t>(&isr110), reinterpret_cast<uintptr_t>(&isr111),
-    reinterpret_cast<uintptr_t>(&isr112), reinterpret_cast<uintptr_t>(&isr113), reinterpret_cast<uintptr_t>(&isr114), reinterpret_cast<uintptr_t>(&isr115), reinterpret_cast<uintptr_t>(&isr116), reinterpret_cast<uintptr_t>(&isr117), reinterpret_cast<uintptr_t>(&isr118), reinterpret_cast<uintptr_t>(&isr119),
-    reinterpret_cast<uintptr_t>(&isr120), reinterpret_cast<uintptr_t>(&isr121), reinterpret_cast<uintptr_t>(&isr122), reinterpret_cast<uintptr_t>(&isr123), reinterpret_cast<uintptr_t>(&isr124), reinterpret_cast<uintptr_t>(&isr125), reinterpret_cast<uintptr_t>(&isr126), reinterpret_cast<uintptr_t>(&isr127),
-    reinterpret_cast<uintptr_t>(&isr128), reinterpret_cast<uintptr_t>(&isr129), reinterpret_cast<uintptr_t>(&isr130), reinterpret_cast<uintptr_t>(&isr131), reinterpret_cast<uintptr_t>(&isr132), reinterpret_cast<uintptr_t>(&isr133), reinterpret_cast<uintptr_t>(&isr134), reinterpret_cast<uintptr_t>(&isr135),
-    reinterpret_cast<uintptr_t>(&isr136), reinterpret_cast<uintptr_t>(&isr137), reinterpret_cast<uintptr_t>(&isr138), reinterpret_cast<uintptr_t>(&isr139), reinterpret_cast<uintptr_t>(&isr140), reinterpret_cast<uintptr_t>(&isr141), reinterpret_cast<uintptr_t>(&isr142), reinterpret_cast<uintptr_t>(&isr143),
-    reinterpret_cast<uintptr_t>(&isr144), reinterpret_cast<uintptr_t>(&isr145), reinterpret_cast<uintptr_t>(&isr146), reinterpret_cast<uintptr_t>(&isr147), reinterpret_cast<uintptr_t>(&isr148), reinterpret_cast<uintptr_t>(&isr149), reinterpret_cast<uintptr_t>(&isr150), reinterpret_cast<uintptr_t>(&isr151),
-    reinterpret_cast<uintptr_t>(&isr152), reinterpret_cast<uintptr_t>(&isr153), reinterpret_cast<uintptr_t>(&isr154), reinterpret_cast<uintptr_t>(&isr155), reinterpret_cast<uintptr_t>(&isr156), reinterpret_cast<uintptr_t>(&isr157), reinterpret_cast<uintptr_t>(&isr158), reinterpret_cast<uintptr_t>(&isr159),
-    reinterpret_cast<uintptr_t>(&isr160), reinterpret_cast<uintptr_t>(&isr161), reinterpret_cast<uintptr_t>(&isr162), reinterpret_cast<uintptr_t>(&isr163), reinterpret_cast<uintptr_t>(&isr164), reinterpret_cast<uintptr_t>(&isr165), reinterpret_cast<uintptr_t>(&isr166), reinterpret_cast<uintptr_t>(&isr167),
-    reinterpret_cast<uintptr_t>(&isr168), reinterpret_cast<uintptr_t>(&isr169), reinterpret_cast<uintptr_t>(&isr170), reinterpret_cast<uintptr_t>(&isr171), reinterpret_cast<uintptr_t>(&isr172), reinterpret_cast<uintptr_t>(&isr173), reinterpret_cast<uintptr_t>(&isr174), reinterpret_cast<uintptr_t>(&isr175),
-    reinterpret_cast<uintptr_t>(&isr176), reinterpret_cast<uintptr_t>(&isr177), reinterpret_cast<uintptr_t>(&isr178), reinterpret_cast<uintptr_t>(&isr179), reinterpret_cast<uintptr_t>(&isr180), reinterpret_cast<uintptr_t>(&isr181), reinterpret_cast<uintptr_t>(&isr182), reinterpret_cast<uintptr_t>(&isr183),
-    reinterpret_cast<uintptr_t>(&isr184), reinterpret_cast<uintptr_t>(&isr185), reinterpret_cast<uintptr_t>(&isr186), reinterpret_cast<uintptr_t>(&isr187), reinterpret_cast<uintptr_t>(&isr188), reinterpret_cast<uintptr_t>(&isr189), reinterpret_cast<uintptr_t>(&isr190), reinterpret_cast<uintptr_t>(&isr191),
-    reinterpret_cast<uintptr_t>(&isr192), reinterpret_cast<uintptr_t>(&isr193), reinterpret_cast<uintptr_t>(&isr194), reinterpret_cast<uintptr_t>(&isr195), reinterpret_cast<uintptr_t>(&isr196), reinterpret_cast<uintptr_t>(&isr197), reinterpret_cast<uintptr_t>(&isr198), reinterpret_cast<uintptr_t>(&isr199),
-    reinterpret_cast<uintptr_t>(&isr200), reinterpret_cast<uintptr_t>(&isr201), reinterpret_cast<uintptr_t>(&isr202), reinterpret_cast<uintptr_t>(&isr203), reinterpret_cast<uintptr_t>(&isr204), reinterpret_cast<uintptr_t>(&isr205), reinterpret_cast<uintptr_t>(&isr206), reinterpret_cast<uintptr_t>(&isr207),
-    reinterpret_cast<uintptr_t>(&isr208), reinterpret_cast<uintptr_t>(&isr209), reinterpret_cast<uintptr_t>(&isr210), reinterpret_cast<uintptr_t>(&isr211), reinterpret_cast<uintptr_t>(&isr212), reinterpret_cast<uintptr_t>(&isr213), reinterpret_cast<uintptr_t>(&isr214), reinterpret_cast<uintptr_t>(&isr215),
-    reinterpret_cast<uintptr_t>(&isr216), reinterpret_cast<uintptr_t>(&isr217), reinterpret_cast<uintptr_t>(&isr218), reinterpret_cast<uintptr_t>(&isr219), reinterpret_cast<uintptr_t>(&isr220), reinterpret_cast<uintptr_t>(&isr221), reinterpret_cast<uintptr_t>(&isr222), reinterpret_cast<uintptr_t>(&isr223),
-    reinterpret_cast<uintptr_t>(&isr224), reinterpret_cast<uintptr_t>(&isr225), reinterpret_cast<uintptr_t>(&isr226), reinterpret_cast<uintptr_t>(&isr227), reinterpret_cast<uintptr_t>(&isr228), reinterpret_cast<uintptr_t>(&isr229), reinterpret_cast<uintptr_t>(&isr230), reinterpret_cast<uintptr_t>(&isr231),
-    reinterpret_cast<uintptr_t>(&isr232), reinterpret_cast<uintptr_t>(&isr233), reinterpret_cast<uintptr_t>(&isr234), reinterpret_cast<uintptr_t>(&isr235), reinterpret_cast<uintptr_t>(&isr236), reinterpret_cast<uintptr_t>(&isr237), reinterpret_cast<uintptr_t>(&isr238), reinterpret_cast<uintptr_t>(&isr239),
-    reinterpret_cast<uintptr_t>(&isr240), reinterpret_cast<uintptr_t>(&isr241), reinterpret_cast<uintptr_t>(&isr242), reinterpret_cast<uintptr_t>(&isr243), reinterpret_cast<uintptr_t>(&isr244), reinterpret_cast<uintptr_t>(&isr245), reinterpret_cast<uintptr_t>(&isr246), reinterpret_cast<uintptr_t>(&isr247),
-    reinterpret_cast<uintptr_t>(&isr248), reinterpret_cast<uintptr_t>(&isr249), reinterpret_cast<uintptr_t>(&isr250), reinterpret_cast<uintptr_t>(&isr251), reinterpret_cast<uintptr_t>(&isr252), reinterpret_cast<uintptr_t>(&isr253), reinterpret_cast<uintptr_t>(&isr254), reinterpret_cast<uintptr_t>(&isr255)
-  };
+        for(size_t i=0; i<IDT_SIZE; ++i)
+          idtEntries[i] = IDTEntry(InterruptType::INTERRUPT_GATE_32, PrivilegeLevel::RING0, 0x8, reinterpret_cast<uintptr_t>(isrs[i]));
+      }
+    };
+    static Initializer initializer;
+
+    return idtEntries;
+  }
+
   Handler handlers[IDT_SIZE] = {};
 
   void pageFaultHandler(uint8_t irqNumber, uint32_t errorCode, uintptr_t oldEip)
@@ -77,7 +77,7 @@ namespace core::interrupts
     io::print("Loading Interrupt Descriptor Table and configuring interrupt handlers...");
 
     // Load idt
-    IDT idt(idtEntries);
+    IDT idt(idtEntries());
     asm volatile ( "lidt %[idt]" : : [idt]"m"(idt) : "ax");
 
     installHandler(8,  &doubleFaultHandler,            PrivilegeLevel::RING0, true);
@@ -112,7 +112,9 @@ namespace core::interrupts
     // TODO: support chaining of handler via additional void* paramter for user
     //       data
     handlers[irqNumber] = handler;
-    idtEntries[irqNumber] = IDTEntry(disableInterrupt ? InterruptType::INTERRUPT_GATE_32 : InterruptType::TRAP_GATE_32, privilegeLevel, 0x8, isrs[irqNumber]);
+    auto& idtEntry = idtEntries()[irqNumber];
+    idtEntry.type(disableInterrupt ? InterruptType::INTERRUPT_GATE_32 : InterruptType::TRAP_GATE_32);
+    idtEntry.privilegeLevel(privilegeLevel);
   }
 
   void uninstallHandler(int irqNumber)
